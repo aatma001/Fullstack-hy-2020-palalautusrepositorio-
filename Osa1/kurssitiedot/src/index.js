e8546daea8e09ea92b02f25a0e950b70d35dfa6f
aatma001 <70138225+aatma001@users.noreply.github.com>
2020-08-26 23:36:39 +0300

    l

---
+++ Osa1/kurssitiedot/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>{props.header}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  const parts = props.parts
+  return (
+    <div>
+        <p><Part parts={parts} index={0}/></p>
+        <p><Part parts={parts} index={1}/></p>
+        <p><Part parts={parts} index={2}/></p>
+    </div>
+  )
+}
+
+  const Total = (props) => {
+
+  const total = props.parts.reduce((a, b) => a + b.exercises, 0)
+
+  return(
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Part = (props) => {
+
+return (
+<div>
+
+{props.parts[props.index].part} {props.parts[props.index].exercises}
+
+</div>
+)
+}
+
+const App = () => {
+
+  const parts = [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+     },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
+];
+
+  return (
+    <div>
+        <Header header='Half Stack application development'/>
+        <Content parts={parts}/>
+        <Total parts={parts}/>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
